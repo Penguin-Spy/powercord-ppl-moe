5de9633b21c497cf3087aec88c5b42ea9d1a6343
Penguin_Spy <penguinspy2435@outlook.com>
2021-05-24 15:02:53 -0700

    Initial commit
    ⚠Warning! May contain: copyright infringement, bad code, security holes

---
+++ README.md
@@ -0,0 +1,20 @@
+# Powercord ppl.moe
+
+This is a powercord plugin that displays a user's ppl.moe profile as a new tab in the user's modal
+
+# Planned features
+
+- [x] Get it working at all
+- [ ] Don't commit copyright infringement
+- [ ] i18n (translations & also emojis in field titles like in the real site)
+- [ ] Display the user's pronouns next to their name in messages (like the pronoundb plugin)
+- [ ] Display the user's badges (Admin, Beta tester, bug reporter [any others?])
+- [ ] Full markdown formatting (tables, using underscores, strikethrough, seperators, code & codeblocks, and more!)
+- [ ] Icons for themes?
+- [ ] Change layout of modal a bit (move connections to their own tab, put notes outside of a tab, have our tab be 1st)?
+- [ ] Display the user's latest post? idk but if people want it i'll add it
+
+# License
+
+yeah idk rn
+this reposotory uses modified snippets of code from https://github.com/cyyynthia/pronoundb-powercord and https://github.com/Ser-Ames/discord-bio
---
+++ index.js
@@ -0,0 +1,208 @@
+const { Plugin } = require('powercord/entities')
+const { inject, uninject } = require('powercord/injector')
+const { React, getAllModules, getModuleByDisplayName } = require('powercord/webpack')
+const { TabBar } = require('powercord/components');
+const { get } = require('powercord/http');
+
+class PplMoe extends Plugin {
+  
+  async fetchAbout(id) {
+    return await get(`https://ppl.moe/api/user/discord/${id}`)
+      .then(r => r.body);
+  }
+  
+  generateInfoDiv(info, key) {
+    if(info[key] != "") { // If this field isn't empty
+      let keyElement;
+      
+      if(key == 'website') {  // If it's the website, make it a link to the text
+        keyElement = React.createElement('div', { className: "marginBottom8-AtZOdT size14-e6ZScH colorStandard-2KCXvj" }, 
+          React.createElement('a', { className: "ppl-moe-link", href: info[key], target: "_blank" }, `${info[key]}`)
+        )
+      } else {
+        keyElement = React.createElement('div', { className: "marginBottom8-AtZOdT size14-e6ZScH colorStandard-2KCXvj" }, `${info[key]}`)
+      }
+      
+      return React.createElement('div', { },
+        React.createElement('div', { className: "userInfoSectionHeader-CBvMDh" }, `${key}`),
+        keyElement
+      )
+    }
+    return null;
+  }
+  
+  generateBioDiv(bioMarkdown) {
+    if(bioMarkdown != "") { // If a bio has been written
+      const bioHTML = bioMarkdown
+      .replace(/"/gim, "&quot;")  // Sanitize HTML stuff (so you can't put XSS in your bio :P
+      .replace(/'/gim, "&apos;")
+      .replace(/</gim, "&lt;")
+      .replace(/>/gim, "&gt;")
+      .replace(/^### (.*$)/gim, '<h3>$1</h3>')  // Format markdown (incomplete, does not include: tables, using underscores, strikethrough, seperators, code & codeblocks, & more!)
+      .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+      .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+      .replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
+      .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
+      .replace(/\*(.*)\*/gim, '<i>$1</i>')
+      .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
+      .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2' target='_blank' class='ppl-moe-link'>$1</a>")
+      .replace(/  $/gim, '<br>')
+      
+      /*<a class="anchor-3Z-8Bb anchorUnderlineOnHover-2ESHQB" title="https://gamebanana.com/mods/49554" href="https://gamebanana.com/mods/49554" rel="noreferrer noopener" target="_blank" role="button" tabindex="0">https://gamebanana.com/mods/49554</a>*/
+      
+      console.log(bioHTML);
+      
+      return React.createElement('div', { },
+        React.createElement('div', { className: "userInfoSectionHeader-CBvMDh" }, `about`),
+        React.createElement('div', { className: "marginBottom8-AtZOdT size14-e6ZScH colorStandard-2KCXvj", dangerouslySetInnerHTML: { __html: bioHTML} })  // whoooo
+      )
+    }
+  }
+  
+  async startPlugin () {
+    this.loadStylesheet('style.css')
+    /*powercord.api.settings.registerSettings('ppl-moe', {
+      category: this.entityID,
+      label: 'ppl.moe',
+      render: Settings
+    })*/
+
+    const _this = this;
+    //const MessageHeader = await this._getMessageHeader()
+    const UserProfile = await this._getUserProfile()
+    const { tabBarItem } = await getAllModules(['tabBarItem'])[1]
+
+    // TODO
+    /*inject('ppl-moe-messages-header', MessageHeader, 'default', function ([ props ], res) {
+      res.props.children[1].props.children.push(
+        React.createElement(
+          'span',
+          { style: { color: 'var(--text-muted)', fontSize: '.9rem', marginRight: props.compact ? '.6rem' : '' } },
+          React.createElement(Pronouns, {
+            userId: props.message.author.id,
+            region: props.message.id === 'pronoundb-fake' ? 'settings' : 'chat',
+            prefix: ' • '
+          })
+        )
+      )
+
+      return res
+    })*/
+    
+    inject('ppl-moe-user-load', UserProfile.prototype, 'componentDidMount', async function (_, res) {
+      const { user } = this.props;
+      if (!user || user.bot) return;
+
+      try {
+        const about = await _this.fetchAbout(user.id);
+        this.setState({ ppl_moe: about });
+      } catch (e) {
+        switch (e.statusCode) {
+          case 404: {
+            this.setState({
+              ppl_moe: {
+                error: "No profile found.",
+                code: e.statusCode
+              }
+            });
+            break;
+          }
+          case 429: {
+            this.setState({
+              ppl_moe: {
+                error: "get rate limited lmao",
+                code: e.statusCode
+              }
+            });
+            break;
+          }
+          default: {
+            console.log(e)
+            this.setState({
+              ppl_moe: {
+                error: "Unknown error, check dev tools console for more info",
+                code: e.statusCode
+              }
+            });
+            break;
+          }
+        }
+      }
+      console.log("[ppl-moe-user-load]:");
+      console.log(this.state.ppl_moe);
+    });
+    
+    inject('ppl-moe-user-tab-bar', UserProfile.prototype, 'renderTabBar', function (_, res) {
+      const { user } = this.props;
+
+      // Do not add a tab if there is no tab bar, no user, or the user's a bot
+      if (!res || !user || user.bot ) return res;
+
+      const bioTab = React.createElement(TabBar.Item, {
+        key: 'PPL_MOE',
+        className: tabBarItem,
+        id: 'PPL_MOE'
+      }, 'ppl.moe');
+
+      // Add the ppl.moe tab bar item to the list
+      res.props.children.props.children.push(bioTab)
+
+      return res;
+    });
+
+    inject('ppl-moe-user-body', UserProfile.prototype, 'render', function (_, res) {
+      // If we're in a different section, don't do anything
+      if (this.props.section !== 'PPL_MOE') return res;
+
+      // Find the body, clear it, & add our info
+      const body = res.props.children.props.children[1];
+      body.props.children = [];
+
+      // ik it's ugly, but it works so :)
+      body.props.children.push(
+        React.createElement('div', { className: "infoScroller-3EYYns thin-1ybCId scrollerBase-289Jih fade-2kXiP2", style: {'overflow': "hidden scroll", 'padding-right': "12px"} },
+          React.createElement('div', { className: "ppl-moe-section" }, [
+            _this.generateInfoDiv(this.state.ppl_moe.info, 'gender'),
+            _this.generateInfoDiv(this.state.ppl_moe.info, 'pronouns'),
+            _this.generateInfoDiv(this.state.ppl_moe.info, 'location'),
+            _this.generateInfoDiv(this.state.ppl_moe.info, 'language'),
+            _this.generateInfoDiv(this.state.ppl_moe.info, 'website'),
+            _this.generateInfoDiv(this.state.ppl_moe.info, 'birthday')
+          ]),
+          React.createElement('div', { className: "ppl-moe-section" }, [
+            _this.generateBioDiv(this.state.ppl_moe.bio),
+          ])
+        )
+      )
+      
+      return res;
+    });
+    
+  }
+
+  pluginWillUnload () {
+    //uninject('ppl-moe-messages-header')
+    uninject('ppl-moe-user-tab-bar')
+    uninject('ppl-moe-user-body')
+  }
+
+  /*async _getMessageHeader () {
+    const d = (m) => {
+      const def = m.__powercordOriginal_default ?? m.default
+      return typeof def === 'function' ? def : null
+    }
+    return getModule((m) => d(m)?.toString().includes('headerText'))
+  }*/
+
+  async _getUserProfile () {
+    const VeryVeryDecoratedUserProfile = await getModuleByDisplayName('UserProfile')
+    const VeryDecoratedUserProfileBody = VeryVeryDecoratedUserProfile.prototype.render().type
+    const DecoratedUserProfileBody = this._extractFromFlux(VeryDecoratedUserProfileBody).render().type
+    const UserProfile = DecoratedUserProfileBody.prototype.render.call({ props: { forwardedRef: null } }).type
+
+    return UserProfile
+  }
+  
+}
+
+module.exports = PplMoe
