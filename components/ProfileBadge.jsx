5ceb0fb61eb51720716ef334bff03c3fc56238f9
Penguin_Spy <penguinspy2435@outlook.com>
2022-08-26 20:32:04 -0700

    fix style of ppl.moe badge, open user modal to profile tab on click also changed VSCode's style options again so rip git diff

--- components/ProfileBadge.jsx
+++ components/ProfileBadge.jsx
@@ -1,18 +1,26 @@
-const { React, Flux, getModuleByDisplayName, i18n: { Messages } } = require('powercord/webpack')
+const { React, Flux, getModule, getModuleByDisplayName, i18n: { Messages } } = require('powercord/webpack')
 const pplMoeStore = require('../store/store.js')
+
 const RoleIcon = getModuleByDisplayName("RoleIcon", false)
+const UserProfileModalActionCreators = getModule(['openUserProfileModal'], false)
 
 function PronounsIcon({ userId, profile }) {
   React.useEffect(() => void pplMoeStore.getProfile(userId), [userId])
 
   // profile not loaded or no pronouns set
-  if (!profile || profile.info?.pronouns == "") return null
+  if(!profile || profile.info?.pronouns == "") return null
 
   return RoleIcon({
     className: "ppl-moe-profile-badge",
     name: Messages.PPL_MOE_OPEN_PROFILE.format({ name: profile.name }),
     onClick: () => {
-      window.open(`https://ppl.moe/u/${encodeURIComponent(profile.unique_url)}`)
+      try {
+        UserProfileModalActionCreators.openUserProfileModal({
+          userId, section: "PPL_MOE"
+        })
+      } catch(e) {
+        console.error(e)
+      }
     },
     src: "https://i.imgur.com/KqbpO1x.png"
   })
--- index.js
+++ index.js
@@ -24,7 +24,7 @@ module.exports = class PplMoe extends Plugin {
       render: Settings
     })
 
-    if (powercord.api.connections.registerConnection) {
+    if(powercord.api.connections.registerConnection) {
       const CONNECTION_TYPE = "ppl-moe"
 
       powercord.api.connections.registerConnection({
@@ -39,7 +39,7 @@ module.exports = class PplMoe extends Plugin {
         fetchAccount: async (id) => {
           const profile = await pplMoeStore.getProfile(id)
 
-          if (!profile) return
+          if(!profile) return
 
           return {
             type: CONNECTION_TYPE,
@@ -72,7 +72,6 @@ module.exports = class PplMoe extends Plugin {
           return true
         }
       })
-      // end of yoinkage
     }
 
     const MessageHeader = await this._getMessageHeader()
@@ -92,15 +91,20 @@ module.exports = class PplMoe extends Plugin {
 
     let memoizedMessageHeader
     inject('ppl-moe-messages-header', MessageHeader, 'default', function (_, res) {
-      if (!memoizedMessageHeader) {
+      if(!memoizedMessageHeader) {
         const originalMessageHeader = res.type
         memoizedMessageHeader = (props) => {
           const res = originalMessageHeader(props)
 
           // unknown author, bot author, or the PronounDB example message in the settings
-          if (!props.message.author.id || props.message.author.bot || props.message.id.includes("pronoundb-fake")) return res
+          if(!props.message.author.id || props.message.author.bot || props.message.id.includes("pronoundb-fake")) return res
 
-          if (powercord.api.settings.store.getSetting("powercord-ppl-moe", "showPronouns", true)) {
+          /*const headerItems = findInReactTree(res,
+            e => Array.isArray(e) && e.find(c => c?.props?.timestamp)
+          )*/
+          const headerItems = res.props.children?.[1]?.props?.children
+
+          if(powercord.api.settings.store.getSetting("powercord-ppl-moe", "showPronouns", true) && headerItems) {
             const hidePronounDB = powercord.api.settings.store.getSetting("powercord-ppl-moe", "hidePronounDB", true)
             const pronouns = React.createElement('span',
               { className: classes.pplMoePronouns + (hidePronounDB ? " " + classes.pplMoePronounsHidePronounDB : "") },
@@ -108,28 +112,29 @@ module.exports = class PplMoe extends Plugin {
                 userId: props.message.author.id
               })
             )
-
-            try { // Attempt to put our span before PronounDB's (so that the CSS can apply)
-              res.props.children[1].props.children.splice(4, 0, pronouns)
-            } catch (e) { // If it fails, just shove it on the end and call it a day, who knows what the array looks like.
-              res.props.children[1].props.children.push(pronouns)
+            // Attempt to put our span before PronounDB's (so that the CSS can apply)
+            const insertIndex = headerItems.findIndex(e => e.props?.className === "pronoundb-pronouns") - 1
+            if(insertIndex >= 0) {
+              headerItems.splice(insertIndex, 0, pronouns)
+            } else { // If it fails, just shove it on the end and call it a day, who knows what the array looks like.
+              headerItems.push(pronouns)
             }
           }
 
-          if (powercord.api.settings.store.getSetting("powercord-ppl-moe", "showProfileBadge", true)) {
-            const profileBadge = React.createElement(ProfileBadge, {
-              userId: props.message.author.id
-            })
-
-            try { // Attempt to put our span before the timestamp
-              res.props.children[1].props.children.splice(3, 0, profileBadge)
-            } catch (e) { // If it fails, just shove it on the end and call it a day, who knows what the array looks like.
-              res.props.children[1].props.children.push(profileBadge)
+          if(powercord.api.settings.store.getSetting("powercord-ppl-moe", "showProfileBadge", true) && headerItems) {
+            /*const usernameItems = findInReactTree(res,
+              e => Array.isArray(e) && e.find(c => c?.props?.message)
+            )*/
+            const usernameItems = headerItems.find(e => e?.props?.id?.startsWith("message-username-"))?.props?.children?.props?.children?.[1]?.props?.children
+            if(usernameItems) {
+              usernameItems.push(React.createElement(ProfileBadge, {
+                userId: props.message.author.id
+              }))
             }
           }
 
           return res
-        }
+        } // end memoizedMessageHeader(props)
       }
 
       res.type = memoizedMessageHeader
@@ -137,7 +142,7 @@ module.exports = class PplMoe extends Plugin {
     })
 
     inject('ppl-moe-tab-bar', TabBar.prototype, 'render', function (_, res) {
-      if (!res.props.className.includes(classes.userProfileTabBar)) return res
+      if(!res.props.className.includes(classes.userProfileTabBar)) return res
 
       const bioTab = React.createElement(TabBar.Item, {
         key: "PPL_MOE",
@@ -163,7 +168,7 @@ module.exports = class PplMoe extends Plugin {
           //console.log(res)  // enable to find UserProfileTabBar modules if console is getting "cant read property 'props' of undefined"
 
           // these must be loaded here because modal classes are lazily loaded
-          if (!classes.lazyLoadedClasses) {
+          if(!classes.lazyLoadedClasses) {
             let userProfileTabBar = getAllModules(['tabBar', 'tabBarItem', 'root'], false)[0]
             let userProfileInfo = getModule(['userInfoSectionHeader'], false)
             let userProfileScroller = getAllModules(['scrollerBase', 'thin'], false)[0]
@@ -180,16 +185,16 @@ module.exports = class PplMoe extends Plugin {
           // fetch their profile for later viewing
           pplMoeStore.getProfile(user.id)
 
-          if (typeof isInjected !== "undefined" && isInjected('ppl-moe-user-profile-tab-selector')) uninject('ppl-moe-user-profile-tab-selector')
+          if(typeof isInjected !== "undefined" && isInjected('ppl-moe-user-profile-tab-selector')) uninject('ppl-moe-user-profile-tab-selector')
           // inject into this instance of the function that decides which tab to display
           // HOW TO FIND: enable console.log(res), search tree through "topSection-" to find a child that has props: { selectedSection: <whatever> }
           //  and a type that's an unnamed function that has a switch statement with cases of `UserProfileSections.foo`
           //  right-click->copy property path of child (not type, the member of the array!), prepend "res." and replace location below
           inject('ppl-moe-user-profile-tab-selector', res.props.children.props.children.props.children[1].props.children, 'type', ([props], res) => {
-            if (props.selectedSection != "PPL_MOE") return res
+            if(props.selectedSection != "PPL_MOE") return res
 
             const profile = pplMoeStore.getProfileSync(user.id)
-            if (!profile) return res  // shouldn't happen (but does), just return the default USER_INFO tab
+            if(!profile) return res  // shouldn't happen (but does), just return the default USER_INFO tab
 
             return React.createElement(Profile, {
               classes: classes,
@@ -198,15 +203,15 @@ module.exports = class PplMoe extends Plugin {
           })
 
 
-          if (typeof isInjected !== "undefined" && isInjected('ppl-moe-user-profile-tab-bar')) uninject('ppl-moe-user-profile-tab-bar')
+          if(typeof isInjected !== "undefined" && isInjected('ppl-moe-user-profile-tab-bar')) uninject('ppl-moe-user-profile-tab-bar')
           // inject into this insance of the tab bar (only after we've rendered it the 1st time)
           // HOW TO FIND: enable console.log(res), search tree through "topSection-" to find a child that has props: { section: <whatever>, setSection: f(e) }
           //  and a type that's function with `displayName: UserProfileTabBar`
           //  right-click->copy property path of child (not type, the member of the array!), prepend "res." and replace location below IN BOTH IF STATEMENT & INJECT
-          if (res.props.children.props.children.props.children[0].props.children[1]) { // if tab bar exists
+          if(res.props.children.props.children.props.children[0].props.children[1]) { // if tab bar exists
             inject('ppl-moe-user-profile-tab-bar', res.props.children.props.children.props.children[0].props.children[1], 'type', ([props], res) => {
               const profile = pplMoeStore.getProfileSync(props.user.id)
-              if (!profile) res.props.children.props.className += ` ${classes.pplMoeDisableTab}`
+              if(!profile) res.props.children.props.className += ` ${classes.pplMoeDisableTab}`
               return res
             })
           }
@@ -219,7 +224,7 @@ module.exports = class PplMoe extends Plugin {
   }
 
   pluginWillUnload() {
-    if (powercord.api.connections.unregisterConnection) {
+    if(powercord.api.connections.unregisterConnection) {
       powercord.api.connections.unregisterConnection('ppl-moe')
     }
     powercord.api.settings.unregisterSettings('ppl-moe')
@@ -245,7 +250,7 @@ module.exports = class PplMoe extends Plugin {
   /* The following code is slightly modified from code found in https://github.com/Juby210/user-details, license/copyright can be found in that repository. */
   async lazyPatchProfileModal(filter, patch) {
     const m = getModule(filter, false)
-    if (m) patch(m)
+    if(m) patch(m)
     else {
       const { useModalsStore } = await getModule(['useModalsStore'])
       inject('ppl-moe-lazy-modal', useModalsStore, 'setState', a => {
@@ -253,14 +258,14 @@ module.exports = class PplMoe extends Plugin {
         a[0] = (...args) => {
           const ret = og(...args)
           try {
-            if (ret?.default?.length) {
+            if(ret?.default?.length) {
               const el = ret.default[0]
-              if (el && el.render && el.render.toString().indexOf(',friendToken:') !== -1) {
+              if(el && el.render && el.render.toString().indexOf(',friendToken:') !== -1) {
                 uninject('ppl-moe-lazy-modal')
                 patch(getModule(filter, false))
               }
             }
-          } catch (e) {
+          } catch(e) {
             this.error(e)
           }
           return ret
